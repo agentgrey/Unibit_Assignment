b5a1a094461757acff92642855b220f85de4dcd0
agentgrey <himadrinayak2702@gmail.com>
2023-06-27 16:01:21 +0530

    minor change

--- solution.java
+++ solution.java
@@ -11,9 +11,9 @@ public class Main {
             System.out.println(Arrays.toString(combination));
         }
 
-        Arrays.sort(nums);
+        int[] newArr = convertToSortedArray(firstCombinations);
 
-        Set<List<Integer>> combinations = multiSum(nums, target*2);
+        Set<List<Integer>> combinations = multiSum(newArr, target*2);
         for (List<Integer> combination : combinations) {
             System.out.println(combination);
         }
@@ -38,6 +38,70 @@ public class Main {
         }
 
         return res;
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = arr[left + i];
+
+
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = arr[mid + 1 + j];
+
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static int[] convertToSortedArray(int[][] arr) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        int[] array1D = new int[rows * cols];
+        int k = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array1D[k++] = arr[i][j];
+            }
+        }
+        mergeSort(array1D, 0, array1D.length - 1);
+        return array1D;
     }
 
     public static Set<List<Integer>> multiSum(int[] nums, int target) {
